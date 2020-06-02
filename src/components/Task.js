02cf985c36691b0ff87e9ad01178615633b65c76
Ishaan <ishaanmukherjee1@gmail.com>
2020-06-02 18:23:20 +0530

    added get tasks functionality and responsiveness

---
+++ src/components/Task.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import dayjs from "dayjs";
+
+// Styles
+import "../styles/Task.css";
+
+class Task extends Component {
+  render() {
+    const {
+      task: { body, createdAt, status, label, taskId },
+    } = this.props;
+
+    // Formatting the status
+    let status_class;
+
+    if (status === "In Progress") {
+      status_class = "in-progress";
+    } else {
+      status_class = status.toLowerCase();
+    }
+
+    return (
+      <div key={taskId} className="task-card">
+        <div className="line-1">
+          <h3 className="task-body">
+            {body}{" "}
+            <span className={"time-stamp"}>
+              posted on {dayjs(createdAt).format("DD/MM/YYYY")}
+            </span>
+          </h3>
+          <div className="other-data">
+            <p className={label.toLowerCase() + " label"}>{label}</p>
+            <p className={status_class + " status-symbol"}>{status}</p>
+          </div>
+        </div>
+        <div className="line-2">
+          <button className="delete-btn edit-btn">Delete</button>
+          <button className="update-btn edit-btn">Update</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+// Components
+import Task from "./Task";
+
+// Styles
 import "../styles/Tasks.css";
 
 class Tasks extends Component {
+  state = {
+    tasks: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/tasks")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          tasks: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
+    // TODO: Loading animation
+
+    let tasksMarkup = this.state.tasks ? (
+      this.state.tasks.map((task) => <Task key={task.taskId} task={task} />)
+    ) : (
+      <p>Loading...</p>
+    );
+
     return (
       <div className="tasks-container">
         <h2 className="mytasks-title">My Tasks</h2>
-        <div className="tasks-list"></div>
+        <div className="tasks-list">{tasksMarkup}</div>
       </div>
     );
   }
