158a0d9ccd5a2da23513231e6dfea68d2d42658e
Ishaan <mukherjeeishaan.2001@gmail.com>
2020-06-04 10:54:05 +0530

    added about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import "../styles/About.css";
 
 class AboutPage extends Component {
   render() {
     return (
-      <div>
-        <p>This is About page</p>
+      <div className="about-page">
+        <h2>Welcome to taskIt</h2>
+        <p>taskIt is a simple task manager app created by Ishaan Mukherjee.</p>
+        <br />
+        <h3>Frameworks used</h3>
+        <h4>Front End</h4>
+        <ul>
+          <li>React JS</li>
+          <li>dayjs</li>
+          <li>Redux</li>
+          <li>Material UI</li>
+        </ul>
+        <h4>Back End</h4>
+        <ul>
+          <li>Express</li>
+          <li>Node JS</li>
+          <li>Firebase</li>
+        </ul>
       </div>
     );
   }
